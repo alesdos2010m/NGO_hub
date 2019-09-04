24c61f773c829a00818eb8a26b710bb08a3492d5
divyapathak24 <42262349+divyapathak24@users.noreply.github.com>
2019-09-04 18:09:21 +0530

    build 0.0.2.2
    Textview button variable in the java code  added

--- NGOHub/app/src/main/java/com/example/ngohub/MainActivity.java
+++ NGOHub/app/src/main/java/com/example/ngohub/MainActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity
 {
   Button login_btn,signup_btn;
+  TextView skip_signin;
 
 
     @Override
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         login_btn =  findViewById(R.id.signin_btn);
         signup_btn = findViewById(R.id.signup_btn);
+        skip_signin=findViewById(R.id.skip_signin);
     }
         public void buttonClick(View v)
         {
