a6c8febd89ead734f48863006b71c83062f165b2
divyapathak24 <42262349+divyapathak24@users.noreply.github.com>
2019-09-04 17:33:21 +0530

    build 0.0.2.1
    Linking MainActivity page with SiginActivity,SignupActivity,HomeActivity.

--- NGOHub/app/src/main/java/com/example/ngohub/MainActivity.java
+++ NGOHub/app/src/main/java/com/example/ngohub/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.ngohub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+{
+  Button login_btn,signup_btn;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login_btn =  findViewById(R.id.signin_btn);
+        signup_btn = findViewById(R.id.signup_btn);
     }
+        public void buttonClick(View v)
+        {
+
+            if(v.getId() == R.id.signup_btn)
+            {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+
+            }
+            else if(v.getId() == R.id.signin_btn)
+            {
+                Intent intent = new Intent(MainActivity.this, SigninActivity.class);
+                startActivity(intent);
+            }
+            else if(v.getId() == R.id.skip_signin)
+            {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+
+        }
+
+
+
 }
